f612258beb3739768f288a8130282175b47ae66c
gudicm <gudicm@gmail.com>
2024-11-06 10:00:00 +0100

    chore: footer foodie decoupled

--- restaurant-app/src/features/Footer/index.tsx
+++ restaurant-app/src/features/Footer/index.tsx
@@ -2,23 +2,16 @@ import React from 'react';
 
 import { SOCIAL_ICONS } from '../../constants/components';
 import SocialLinks from '../SocialLinks';
+import Foodie from './Foodie';
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer" id="footer">
       <div className="footer-top" style={{ backgroundImage: "url('./src/assets/footer-illustration.png')" }}>
         <div className="container">
-          <div className="footer-brand">
-            <a href="" className="logo">
-              Foodie<span className="span">.</span>
-            </a>
 
-            <p className="footer-text">
-              Financial experts support or help you to to find out which way you can raise your funds more.
-            </p>
+          <Foodie />
 
-            <SocialLinks items={SOCIAL_ICONS} />
-          </div>
         </div>
       </div>
       <div className="footer-bottom">
